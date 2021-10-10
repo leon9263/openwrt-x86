f0738504e827279ecf1256c7a0cd34ce75cf9ce1
leon9263 <91870699+leon9263@users.noreply.github.com>
2021-10-10 11:30:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'>>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small'>>feeds.conf.default
 git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/coolsnowwolf/lede
